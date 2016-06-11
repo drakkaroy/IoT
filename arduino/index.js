cd0b862c21d224cb05fb8e5d951526a036868864
Rodolfo Oviedo <rodolfo.oviedo@digitaslbi.com>
2016-06-11 01:30:05 -0600

    Adding firebase integration to arduino

--- arduino/index.js
+++ arduino/index.js
@@ -1,15 +1,47 @@
 var five = require("johnny-five");
 var board = new five.Board();
+var firebase = require("firebase");
+var firebaseDB;
+var myFirebaseRef;
+var colors = {
+	'red': 'FF0000',
+	'orange': 'FF7F00',
+	'yellow': 'FFFF00',
+	'green': '00FF00',
+	'blue': '0000FF',
+	'indigo': '4B0082',
+	'violet': '8B00FF'
+};
 
 board.on("ready", function() {
   var rgb = new five.Led.RGB([9, 10, 11]);
-  var index = 0;
-  var rainbow = ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "8F00FF"];
 
-  this.loop(1000, function() {
-    if (index + 1 === rainbow.length) {
-      index = 0;
-    }
-    rgb.color(rainbow[index++]);
+  firebase.initializeApp({
+	databaseURL: "https://iot-project-2d8c5.firebaseio.com",
+	serviceAccount: "IoT Project-5de2f8ef7beb.json"
+  });
+
+  firebaseDB = firebase.database();
+  myFirebaseRef = firebaseDB.ref("color");
+
+  // var index = 0;
+  // var rainbow = ["FF0000", "FF7F00", "FFFF00", "00FF00", "0000FF", "4B0082", "8B00FF"];
+
+  // this.loop(1000, function() {
+  //   rgb.color(rainbow[index]);
+  //   index++;
+
+  //   if (index === rainbow.length) {
+  //     index = 0;
+  //   }
+  // });
+
+  myFirebaseRef.on("value", function(snapshot) {
+	var color = snapshot.val();
+	console.log(color);
+
+	if (colors[color]) {
+	  rgb.color(colors[color]);
+	}
   });
 });
